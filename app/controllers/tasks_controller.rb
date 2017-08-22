3b9a7214b7d5a44508ab0ddd8c0a59fa30c4409f
Devan Kestel <devan.kestel@gmail.com>
2017-08-21 23:18:41 -0400

    New tasks can be created with existing categories via web form.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,22 @@
 class TasksController < ApplicationController
-    def show
+    def index
         @tasks = Task.all
     end
+    def new
+        @task = Task.new
+    end
+    def create
+        Task.create!(task_params)
+        redirect_to action: "index"
+    end
+    def update
+
+    end
+    def destroy
+
+    end
+    def task_params
+        params.require(:task).permit(:title, :category_id)
+    end
+
 end
